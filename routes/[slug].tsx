c0d077719bea01fd09f49c369e4b22796e7c88ba
Jacques Desmarais <jdesma@gmail.com>
2023-08-23 09:53:49 -0400

    enable markdown in posts

--- routes/[slug].tsx
+++ routes/[slug].tsx
@@ -1,4 +1,6 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
+import { Head } from "$fresh/runtime.ts";
+import { CSS, render } from "$gfm";
 import { getPost, Post } from "../src/posts/index.ts";
 
 export const handler: Handlers<Post> = {
@@ -12,16 +14,24 @@ export const handler: Handlers<Post> = {
 export default function PostPage(props: PageProps<Post>) {
   const post = props.data;
   return (
-    <main class="max-w-screen-md px-4 pt-16 mx-auto">
-      <h1 class="text-5xl font-bold">{post.title}</h1>
-      <time class="text-gray-500">
-        {new Date(post.publishedAt).toLocaleDateString("en-us", {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })}
-      </time>
-      <div class="mt-8" dangerouslySetInnerHTML={{ __html: post.content }} />
-    </main>
+    <>
+      <Head>
+        <style dangerouslySetInnerHTML={{ __html: CSS }} />
+      </Head>
+      <main class="max-w-screen-md px-4 pt-16 mx-auto">
+        <h1 class="text-5xl font-bold">{post.title}</h1>
+        <time class="text-gray-500">
+          {new Date(post.publishedAt).toLocaleDateString("en-us", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </time>
+        <div
+          class="mt-8 markdown-body"
+          dangerouslySetInnerHTML={{ __html: render(post.content) }}
+        />
+      </main>
+    </>
   );
 }
